ccd91c31f8343e58f49b66d95f15754add1b070c
Brian Fontana <brian@codefoxtrot.com>
2021-03-09 09:25:31 -0500

    Add project files.

---
+++ WpfCalculator/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfCalculator.ViewModels.Commands;
+
+namespace WpfCalculator.ViewModels
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private string _resultDisplay;
+        /// <summary>
+        /// The main display of the calculator.
+        /// </summary>
+        public string ResultDisplay
+        {
+            get { return _resultDisplay; }
+            set 
+            { 
+                _resultDisplay = value;
+                OnPropertyChanged("ResultDisplay");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ButtonCommand CalculatorCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Implementation of INotifyPropertyChanged, for notifying subscribers to property changes.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Default constructor to initialize main display of the calculator.
+        /// </summary>
+        public CalculatorViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                _resultDisplay = "8008";
+            }
+            else
+            {
+                _resultDisplay = "0";
+            }
+
+            CalculatorCommand = new ButtonCommand(this);
+        }
+
+        public void PerformOperation(string value)
+        {
+            ResultDisplay = value;
+        }
+    }
+}
---
+++ WpfCalculator/ViewModels/Commands/ButtonCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfCalculator.ViewModels.Commands
+{
+    public class ButtonCommand : ICommand
+    {
+        public CalculatorViewModel CalculatorVM { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public ButtonCommand(CalculatorViewModel calculatorViewModel)
+        {
+            CalculatorVM = calculatorViewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            string buttonCmd = parameter as string;
+
+            if (string.IsNullOrWhiteSpace(buttonCmd))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            string buttonCmd = parameter as string;
+            CalculatorVM.PerformOperation(buttonCmd);
+        }
+    }
+}
